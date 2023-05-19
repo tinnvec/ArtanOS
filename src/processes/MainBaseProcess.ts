900bc54aefa6536aa6f3a692d17f847628577a5c
Alex Hanna <tinnvec@gmail.com>
2023-05-19 11:58:48 -0500

    Clean up MainBaseProcess

--- src/processes/MainBaseProcess.ts
+++ src/processes/MainBaseProcess.ts
@@ -1,86 +1,20 @@
-import { Process, registerProcess } from 'Kernel';
-import { SimpleRoomPosition } from 'shared/Types';
+import { LayoutPlanner } from 'components/LayoutPlanner';
+import { Kernel, Process, registerProcess } from 'Kernel';
 import { Logger } from 'utils/Logger';
 
 //#region Types
 
 type MainBaseProcessMemory = {
-  harvesterPositions: {
-    local: SimpleRoomPosition[]
-  };
   roomName: string;
-  sourceIDs: {
-    local: string[]
-  };
-  spawnIDs: string[];
-}
+};
 
 //#endregion
 
 @registerProcess
 export class MainBaseProcess extends Process {
   public memory: MainBaseProcessMemory = {
-    harvesterPositions: {
-      local: []
-    },
-    roomName: '',
-    sourceIDs: {
-      local: []
-    },
-    spawnIDs: []
+    roomName: ''
   };
-
-  private _localHarvesterPositions?: RoomPosition[];
-  private get localHarvesterPositions(): RoomPosition[] {
-    if (this._localHarvesterPositions) {
-      return this.localHarvesterPositions;
-    }
-
-    this._localHarvesterPositions = [];
-
-    if (this.memory.harvesterPositions.local.length > 0) {
-      for (const position of this.memory.harvesterPositions.local) {
-        const roomPosition = new RoomPosition(position.x, position.y, position.roomName);
-        this._localHarvesterPositions.push(roomPosition);
-      }
-
-      return this.localHarvesterPositions;
-    }
-
-    //
-
-    return this._localHarvesterPositions;
-  }
-
-  private _localSources?: Source[];
-  private get localSources(): Source[] {
-    if (this._localSources) {
-      return this._localSources;
-    }
-
-    this._localSources = [];
-
-    if (this.memory.sourceIDs.local.length > 0) {
-      for (const sourceID of this.memory.sourceIDs.local) {
-        const source = Game.getObjectById(sourceID as Id<Source>);
-
-        if (!source) {
-          const index = this.memory.sourceIDs.local.indexOf(sourceID);
-          this.memory.sourceIDs.local.splice(index);
-          continue;
-        }
-
-        this._localSources.push(source);
-      }
-
-      return this._localSources;
-    }
-
-    this._localSources = this.room.find(FIND_SOURCES);
-    this.memory.sourceIDs.local = this._localSources.map(_ => _.id);
-
-    return this._localSources;
-  }
 
   private _room?: Room;
   private get room(): Room {
@@ -92,45 +26,29 @@ export class MainBaseProcess extends Process {
     return this._room;
   }
 
-  private _spawns?: StructureSpawn[];
-  private get spawns(): StructureSpawn[] {
-    if (this._spawns) {
-      return this._spawns;
-    }
-
-    this._spawns = [];
-
-    if (this.memory.spawnIDs.length > 0) {
-      for (const spawnID of this.memory.spawnIDs) {
-        const spawn = Game.getObjectById(spawnID as Id<StructureSpawn>);
-
-        if (!spawn) {
-          const index = this.memory.spawnIDs.indexOf(spawnID);
-          this.memory.spawnIDs.splice(index);
-          continue;
-        }
-
-        this._spawns.push(spawn);
-      }
-
-      return this._spawns;
-    }
-
-    this._spawns = this.room.find(FIND_MY_SPAWNS);
-    this.memory.spawnIDs = this._spawns.map(_ => _.id);
-
-    return this._spawns;
-  }
-
   public run(): void {
     if (this.memory.roomName === '') {
       Logger.debug('No roomName in MainBaseProcess memory, killing process');
-      return this.stop();
+      return Kernel.killProcess(this.pid);
     }
 
-      this.room = Game.rooms[this.memory.roomName];
-    }
+    LayoutPlanner.generateLayout(this.room.name);
 
-    Logger.debug(`MainBaseProcess for ${this.room.name}`);
+    // Temp first room code
+
+    // const starterBody = [CARRY, WORK, MOVE, MOVE];
+
+    // for (const spawn of this.spawns) {
+    //   if (spawn.spawning) {
+    //     continue;
+    //   }
+
+    //   const result = spawn.spawnCreep(starterBody, Game.time.toString());
+    //   Logger.debug(`Spawn result: ${result}`);
+    // }
   }
+
+  //#region Private Methods
+
+  //#endregion
 }
